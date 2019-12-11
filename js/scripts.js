eb77a6c821ef0616aa05e2b822a313e26bb02929
Megan Kaluza <megankaluza@Megans-MacBook-Pro.local>
2019-12-11 14:12:04 -0800

    fix scrolling affect

--- js/scripts.js
+++ js/scripts.js
@@ -4,116 +4,117 @@ ScrollReveal().reveal('.revealOne', { delay: 400 });
 ScrollReveal().reveal('.revealTwo', { delay: 800 });
 ScrollReveal().reveal('.revealThree', { delay: 1200 });
 ScrollReveal().reveal('.revealFour', { delay: 1600 });
-
-$(document).ready(function(){
-  //Smooth Scrolling
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-
-  //Open Modal
-  $('#open').click(function(){
-    $('.ten')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  //Exit Modal
-  $('.exit').click(function(){
-    $('.modal').modal('hide');
-  });
-  $('#openFeralBlack').click(function(){
-    $('.feralBlack')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openFeralWhite').click(function(){
-    $('.feralWhite')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openInsta').click(function(){
-    $('.insta')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openBody').click(function(){
-    $('.body')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openTumblr').click(function(){
-    $('.tumblr')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openAdidas').click(function(){
-    $('.adidas')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openMobile').click(function(){
-    $('.mobile')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openDetox').click(function(){
-    $('.detox')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openOriginals').click(function(){
-    $('.originals')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openAesthetic').click(function(){
-    $('.aesthetic')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openSpecialist').click(function(){
-    $('.specialist')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#openSpecialistBlack').click(function(){
-    $('.specialistBlack')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-});
+// 
+// $(document).ready(function(){
+//   //Smooth Scrolling
+//   $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+//
+//   // Open Modal
+//   $('#open').click(function(){
+//     $('.ten')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   // Exit Modal
+//   $('.exit').click(function(){
+//     $('.modal').modal('hide');
+//   });
+//   $('#openFeralBlack').click(function(){
+//     $('.feralBlack')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openFeralWhite').click(function(){
+//     $('.feralWhite')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openInsta').click(function(){
+//     $('.insta')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openBody').click(function(){
+//     $('.body')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openTumblr').click(function(){
+//     $('.tumblr')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openAdidas').click(function(){
+//     $('.adidas')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openMobile').click(function(){
+//     $('.mobile')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openDetox').click(function(){
+//     $('.detox')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openOriginals').click(function(){
+//     $('.originals')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openAesthetic').click(function(){
+//     $('.aesthetic')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openSpecialist').click(function(){
+//     $('.specialist')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//   $('#openSpecialistBlack').click(function(){
+//     $('.specialistBlack')
+//     .modal({
+//       blurring: true
+//     })
+//     .modal('show');
+//   });
+//
+// });
